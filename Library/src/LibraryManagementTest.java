19f0aa71a2bc2aa8efe945f85cb604ba267f3c4e
aaronsarjeant <aaronsarjeant@trentu.ca>
2024-11-25 23:23:43 -0500

    Task 3 Part 2

--- Library/src/LibraryManagementTest.java
+++ Library/src/LibraryManagementTest.java
@@ -12,5 +12,26 @@ public class LibraryManagementTest {
 		Book book4 = new Book(99, "book4");
 		Book book5 = new Book(204857, "book5");
 	}
+@Test
+public void testBorrowReturn() throws Exception
+{
+	Book book1 = new Book(100, "book1");
+	Member member1 = new Member(200, "member1");
+	assertTrue(book1.isAvailable());
+	Transaction t = Transaction.getTransaction();
+	assertTrue(t.borrowBook(book1, member1));
+	assertFalse(book1.isAvailable());
+	assertFalse(t.borrowBook(book1, member1));
+	assertTrue(t.returnBook(book1, member1));
+	assertTrue(book1.isAvailable());
+	assertFalse(t.returnBook(book1, member1));
+	
+	
+}
+
 
 }
+
+
+	
+
